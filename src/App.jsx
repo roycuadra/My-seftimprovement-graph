5423340504306a1645d757009a5d41cd3559aa77
roycuadra <103763740+roycuadra@users.noreply.github.com>
2025-03-31 00:12:12 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ const LearningGrowthGraph = () => {
   useEffect(() => {
     // Data for the graph
     const data = {
-      labels: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
+      labels: ['2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025'],
       datasets: [
         {
           label: 'Learning and Growth', // Updated label
           borderColor: 'rgb(75, 192, 192)',
-          data: [5, 45, 25, 10, 15, 45, 50], // Replace with your actual data
+          data: [5, 45, 25, 10, 15, 45, 50, 70, 100], // Replace with your actual data
           fill: false,
         },
       ],
